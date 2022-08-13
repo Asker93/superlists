5ffb85381f81728bde0062280d7717e9a83e037b
ASKER <sallybaev93@mail.ru>
2022-08-13 23:23:27 +0300

    Первы модульный тест и преобразование url, фиктивное представление

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    '''тест на токсичность'''
+class HomePageTest(TestCase):
+    '''Тест домашней страницы'''
 
-    def test_bad_maths(self):
-        '''Тест: неправильные математические расчеты'''
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        '''Тест корневой URL преобразуется в представление домашней страницы'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lists import views
+from django.urls import re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    re_path('^$', views.home_page, name='home'),
 ]
