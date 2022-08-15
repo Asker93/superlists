cf4fc1fad161f4a0eba37859ba4655205f85196d
ASKER <sallybaev93@mail.ru>
2022-08-15 22:18:11 +0300

    Добавлена новая модель дыннх List

--- lists/models.py
+++ lists/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 
 
+class List(models.Model):
+    '''Списки'''
+    pass
+
 class Item(models.Model):
     '''Элемент списка'''
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
